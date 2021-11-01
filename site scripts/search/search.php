409894b1bb9e47aeadba92bc9af67712ba1f5a14
saks <prohapro@gmail.com>
2021-11-01 16:35:03 +0200

    Небольшое изменение серверных скриптов

--- site scripts/search/search.php
+++ site scripts/search/search.php
@@ -9,17 +9,11 @@ $posts = $jfo;
 $count = 1;
 $apps = array();
 foreach ($posts as $post) {
-    //echo $post->name;
-    //echo '<br>';
-
     $pos = strpos($post->name, $search_word);
-
     if ( $pos !== false ) {
-       //echo "<b>found $count</b>";
        array_push($apps, $count);
     }
     $count++;
-
 }
 echo json_encode($apps);
 ?>
